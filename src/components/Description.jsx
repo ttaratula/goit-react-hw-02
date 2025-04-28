8e77b2edec0b99c005b007b9b6484162a8739ca3
Tetiana Taratula <ttaratula@gmail.com>
2025-04-28 20:32:44 +0100

    alldone

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,7 +1,7 @@
 
 export default function Description (){
     return (<>
-      <h1>Sip Happens Caféeeeee</h1>
+      <h1>Sip Happens Café</h1>
       <p>Please leave your feedback about our service by selecting one of the options below.</p>
     </>)
 
